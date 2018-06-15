e96dddec633b45ed1849bc46b327e8cbd6968f2a
Farhan <farhangdon@gmail.com>
2018-06-15 10:36:32 +0200

    Assignment 2

---
+++ web information systems asssignment 2/assignment 2/assignment2Task3/assignment2Task3/src/main/java/com/hammad/assignment2/SchoolService.java
@@ -0,0 +1,20 @@
+package com.hammad.assignment2;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping(value = "/rest/school")
+class SchoolService {
+
+    @RequestMapping(value = "/students/{id}")
+    public Student getStudent(@PathVariable String id) {
+        String URL = "http://localhost:8091/rest/student/" + id;
+        RestTemplate template = new RestTemplate();
+        Student student = template.getForObject(URL, Student.class);
+        return student;
+    }
+}
